b2d7a94e48557ae3eadd55e7853e0f8977d424ca
Gabriel Soares <gabrielso4res@gmail.com>
2021-04-29 09:09:43 -0300

    debug configurado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import * as express from 'express';
+import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello' });
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
 });
 
 app.listen(3333, () => console.log('Server is runnig!'));
